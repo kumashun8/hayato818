474968f2fa253c4056d59eb81fd46bc96238d38d
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-04 00:08:53 +0900

    set profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,7 +7,12 @@ class Profile extends React.Component {
         return (
             <div className={Styles.Profile}>
                 <Headline name={"Profile"} />
-                <p className={Styles.Profile__text}>大隈隼 (おおくまはやと). 1997年生まれで福岡出身. 2018年に北九州高専を卒業し, 九州工業大学情報工学部 知能情報工学科(飯塚市)へ編入学しました. 現在は学部4年で, 坂本研究室に所属しており, 2020年度からは東京でWebエンジニアとして働く予定です.</p>
+                <p className={Styles.Profile__text}>
+                    大隈隼 (おおくまはやと). 1997年生まれで福岡出身. 2018年に北九州高専を卒業し, 九州工業大学情報工学部 知能情報工学科(飯塚市)へ編入学しました. 現在は学部4年で坂本研究室に所属しており, 圧縮アルゴリズムに関する研究に取り組んでいます. 
+                </p>
+                <p className={Styles.Profile__text}>
+                    また, アプリケーションやWebサービスの開発などを行うIT学生団体 P&D に所属し, 日々技術の習得や団体としての活動にも参加しています. そうした実践的な経験を活かして, 2020年度からは東京でWebエンジニアとして働く予定です.
+                </p>
             </div>
         );
     }
