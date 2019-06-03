658b9091261ded52fdb9218aaa4bbbe497835f68
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-04 00:47:51 +0900

    set skill

---
+++ src/components/skill.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Headline from './headline';
+import Styles from '../styles/components/skill.module.scss'
+
+const skillList = [
+    "Ruby on Rails", "React", "GatsbyJS"
+];
+
+class Skill extends React.Component {
+    
+    render() {
+        return (
+            <div className={Styles.Skill}>
+                <Headline name={"Skill"} />
+                <ul className={Styles.Skill__text}>
+                    {skillList.map((skillItem) => (
+                        <li>{skillItem}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Skill
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,13 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Styles from "../styles/pages/index.module.scss"
 import Profile from "../components/profile"
+import Skill from "../components/skill"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Hayato OKUMA" />
     <Profile />
+    <Skill />
   </Layout>
 )
 
