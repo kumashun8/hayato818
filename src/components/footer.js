468d185b4b5228b09e2481465026f9adfe0336ea
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-04 16:44:06 +0900

    fix account

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Styles from '../styles/components/footer.module.scss'
 
+const accountList = [
+    {
+        icon: "fab fa-facebook-square",
+        link: "https://www.facebook.com/ohayato9",
+        text: "ohayato9"
+    },
+    {
+        icon: "fab fa-facebook-square",
+        link: "https://github.com/8810KMKM",
+        text: "KMKM8810"
+    },
+    {
+        icon: "fas fa-envelope-square",
+        link: "mailto:p00h_33@outlook.jp",
+        text: "p00h_33@outlook.jp"
+    }
+];
+
 class Footer extends React.Component {
     render() {
         return (
@@ -16,9 +34,9 @@ class Footer extends React.Component {
                 </div>
                 <div className={Styles.footer__account}>
                     <ul className={Styles.footer__account__list}>
-                        <li><a href="https://www.facebook.com/ohayato9"><i class="fab fa-facebook-square"></i>ohayato9</a></li>
-                        <li><a href="https://github.com/8810KMKM"><i class="fab fa-github-square"></i>KMKM8810</a></li>
-                        <li><a href="mailto:p00h_33@outlook.jp"><i class="fas fa-envelope-square"></i>p00h_33@outlook.jp</a>  </li>
+                        {accountList.map((item) => (
+                            <li><a href={item.link} target="_blank"><i className={item.icon}></i>{item.text}</a></li>
+                        ))}
                     </ul>
                 </div>
             </footer>
