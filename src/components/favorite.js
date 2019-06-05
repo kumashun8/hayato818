8e9a8abbdfe2d460fa4d44793c23457cdaab5d6b
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-05 20:42:13 +0900

    add favorite contents

--- src/components/favorite.js
+++ src/components/favorite.js
@@ -2,21 +2,47 @@ import React from 'react';
 import Headline from './headline';
 import Styles from '../styles/components/favorite.module.scss'
 
-const skillList = [
-    "Ruby on Rails", "React", "GatsbyJS", "Illustrator (ちょっと)"
+const favoList = [
+    {
+        category: "Food",
+        object: "チキン南蛮, ガトーショコラ"
+    },
+    {
+        category: "Drink",
+        object: "ブラックコーヒー"
+    },
+    {
+        category: "Music",
+        object: "サカナクション"
+    },
+    {
+        category: "Actress",
+        object: "中条あやみ"
+    },
+    {
+        category: "Color",
+        object: "チャコールグレー"
+    },
+    {
+        category: "Fashion brand",
+        object: "MONKEY TIME, Jieda, NIKE"
+    },
 ];
 
 class Favorite extends React.Component {
     
     render() {
         return (
-            <div className={Styles.Skill}>
-                <Headline name={"Skill"} />
-                <ul className={Styles.Skill__text}>
-                    {skillList.map((skillItem) => (
-                        <li>{skillItem}</li>
+            <div className={Styles.Favorite}>
+                <Headline name={"Favorite"} />
+                <table className={Styles.Favorite__item} bor>
+                    {favoList.map((favoItem) => (
+                        <tr>
+                            <td className={Styles.Favorite__item__category}>{favoItem.category}</td>
+                            <td className={Styles.Favorite__item__object}>{favoItem.object}</td>
+                        </tr>
                     ))}
-                </ul>
+                </table>
             </div>
         );
     }
