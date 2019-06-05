7a00307e3e7cc7a5c914cf453fd4921960b4d59d
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-05 19:44:28 +0900

    add work content

--- src/components/workItem.js
+++ src/components/workItem.js
@@ -5,13 +5,13 @@ import Image from "../components/image"
 class WorkItem extends React.Component {
     render() {
         return (
-            <div className={Styles.WorkItem}>
-                <div className={Styles.WorkItem__image}>
-                    <Image filename={this.props.image} />
+            <a href={this.props.data.link} target="_blank" className={Styles.WorkItem}>
+                <div  className={Styles.WorkItem__image}>
+                    <Image filename={this.props.data.image} />
                 </div>
-                <h2 className={Styles.WorkItem__title}>{this.props.title}</h2>
-                <p className={Styles.WorkItem__text}>{this.props.text}</p>
-            </div>
+                <h2 className={Styles.WorkItem__title}>{this.props.data.title}</h2>
+                <p className={Styles.WorkItem__text}>{this.props.data.text}</p>
+            </a>
         );
     }
 }
