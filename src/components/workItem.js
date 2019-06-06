e05ef00e578540248b65e73177cd2985676ee29d
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2019-06-06 15:28:50 +0900

    fix work title

--- src/components/workItem.js
+++ src/components/workItem.js
@@ -6,10 +6,10 @@ class WorkItem extends React.Component {
     render() {
         return (
             <a href={this.props.data.link} target="_blank" className={Styles.WorkItem}>
-                <div  className={Styles.WorkItem__image}>
+                <h2 className={Styles.WorkItem__title}>{this.props.data.title}</h2>
+                <div className={Styles.WorkItem__image}>
                     <Image filename={this.props.data.image} />
                 </div>
-                <h2 className={Styles.WorkItem__title}>{this.props.data.title}</h2>
                 <p className={Styles.WorkItem__text}>{this.props.data.text}</p>
             </a>
         );
